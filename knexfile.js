91b6d612e0d3f42d468fde4a5f3d5b3b4dcfc970
xwrexx <wrex1285@gmail.com>
2018-12-06 10:20:42 -0700

    set up knex file for express app

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql://localhost/MOVIES_CRUD_PROJECT'
+  },
+
+  production: {
+    client: 'pg',
+    connection: 'process.env.DATABASE_URL',
+  }
+
+};
